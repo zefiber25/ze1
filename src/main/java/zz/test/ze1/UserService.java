df5f15f97a3683f885dcb80359e6ad107485d1eb
Ze Wang <Ze.Wang@pointclickcare.com>
2018-05-28 16:44:12 -0400

    create new file and modify property file

---
+++ src/main/java/zz/test/ze1/UserService.java
@@ -0,0 +1,17 @@
+package zz.test.ze1;
+
+import org.springframework.stereotype.Service;
+
+@Service("UserService")
+public class UserService
+{
+
+  // @Value("${zeSignature}")
+  private String zeSignature;
+
+  public String getSignature()
+  {
+    return "Proudly handcrafted by ZZ ! :)";
+  }
+
+}
